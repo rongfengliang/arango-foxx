d64a8e379f40b92d4793f33fa1cf3626d5b657c6
rongfengliang <1141591465@qq.com>
2017-12-18 15:01:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+use strict';
+const createRouter = require('@arangodb/foxx/router');
+const router = createRouter();
+router.get('/hello-world', function (req, res) {
+  res.send('Hello World!');
+})
+.response(['text/plain'], 'A generic greeting.')
+.summary('Generic greeting')
+.description('Prints a generic greeting.');
+module.context.use(router);
